4feae3a11057421bb6d5bf47e0ef8cba6f3317fb
Young Woo Nam <spow2544@gmail.com>
2021-06-10 12:52:09 +0900

    remove not used function

--- utils.py
+++ utils.py
@@ -62,16 +62,3 @@ def logsoftmax_2d(logits):
         logits.reshape(list(logits.shape[:-2]) + [-1])).reshape(orig_shape)
     return logprobs
 
-def run_func_in_parts(func, vid_emb, aud_emb, part_len, dim, device):
-    """
-    Run given function in parts, spliting the inputs on dimension dim 
-    This is used to save memory when inputs too large to compute on gpu 
-    """
-    dist_chunk = []
-    for v_spl, a_spl in tqdm(list(
-            zip(vid_emb.split(part_len, dim=dim),
-                aud_emb.split(part_len, dim=dim))),
-                             desc='Calculating pairwise scores'):
-        dist_chunk.append(func(v_spl.to(device), a_spl.to(device)))
-    dist = torch.cat(dist_chunk, dim - 1)
-    return dist
